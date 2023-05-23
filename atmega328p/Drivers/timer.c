b4cf2de3464c5d08e8813125876f49ce064c264d
Andrei Simion <andrei.simion1707@upb.ro>
2023-05-23 12:21:55 +0300

    WIP Driver Timer: add 8b timer/counter 0  CTC mode implementation

--- atmega328p/Drivers/timer.c
+++ atmega328p/Drivers/timer.c
@@ -1,4 +1,4 @@
-#include<timer.h>
+#include "timer.h"
 #include<avr/io.h>
 #include<avr/interrupt.h>
 
@@ -15,29 +15,44 @@ struct timer init_struct_timerstruct timer create_struct_timer(uint8_t sel, uint
 }
 
 void init_timer(struct timer _timer){
+       	TCCR0A = 0; //clear 
+	TCCR0B = 0; //clear
+	TIMSKO = 0; //clear
+	TIFR0  = 0; //clear        
         if(_timer.sel == 0){
                 //first timer/counter 0
        		switch(_timer.mode){
        			case 0://Normal mode
-       				TCCR0A = 0; //clear --> set Normal Mode
-				TCCR0B = 0; //clear
-				TIMSKO = 0; //clear
-				TIFR0  = 0; //clear
-				
+       				TCCR0A = 0; //set Normal Mode	
+       				
 	                        if(_timer.prescaler_frq == 5)       TCCR0B |= (1 << CS02) | (1 << CS00);
 				else if(_timer.prescaler_frq == 4)  TCCR0B |= (1 << CS02);
 				else if(_timer.prescaler_frq == 3)  TCCR0B |= (1 << CS01) | (1 << CS00);
 				else if(_timer.prescaler_frq == 2)  TCCR0B |= (1 << CS01);
 				else if(_timer.prescaler_frq == 1)  TCCR0B |= (1 << CS00);
 				
-				OCR0A = _timer.cmp_val;
+				TOP = 0xFF;
+				OCR0A = 0xFF;
 				
 				TIMSKO |= (1 << OCIE0A);
 				sei(); // activate global interrupt 
 				break;
-			case 1://CTC mode
+			case 1://PWM Phase Correct classic mode
+			       
 			       break;
-			case 2://Fast PWM mode
+			case 2://CTC
+			       TCCR0A |= (1 << WGM01); //set CTC Mode
+	                       
+	                       if(_timer.prescaler_frq == 5)       TCCR0B |= (1 << CS02) | (1 << CS00);
+			       else if(_timer.prescaler_frq == 4)  TCCR0B |= (1 << CS02);
+			       else if(_timer.prescaler_frq == 3)  TCCR0B |= (1 << CS01) | (1 << CS00);
+			       else if(_timer.prescaler_frq == 2)  TCCR0B |= (1 << CS01);
+			       else if(_timer.prescaler_frq == 1)  TCCR0B |= (1 << CS00);
+			       
+			       OCR0A = _timer.cmp_val;
+			       
+			       TIMSKO |= (1 << OCIE0A);
+			       sie(); //activate global interrupt
 			       break;
 			case 3://Phase Correct PWM mode
 			      break;
@@ -45,10 +60,10 @@ void init_timer(struct timer _timer){
         	
         	
         }else if(_timer.sel == 1){
-            //second timer/counter 1
+            //16b timer/counter
         
         }else if(_timer.sel == 2){
-          //for 16b timer/counter
+          //second 8b timer/counter
         }
 	
 }
