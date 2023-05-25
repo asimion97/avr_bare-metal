b30176f0bc84f295ab0ed884377ffc6a1d0367f5
Andrei Simion <andrei.simion1707@upb.ro>
2023-05-25 14:48:14 +0300

    WIP Driver Timer: refactor and add support to the timer/counter 2

--- atmega328p/Drivers/timer.c
+++ atmega328p/Drivers/timer.c
@@ -2,38 +2,25 @@
 #include<avr/io.h>
 #include<avr/interrupt.h>
 
-struct timer init_struct_timerstruct timer create_struct_timer(uint8_t sel, uint8_t mode, uint8_t pres_frq, uint8_t cmp_val, uint8_t top_val, uint8_t bot_val){
-	struct timer my_timer;
-	my_timer.select = sel;
-	my_timer.mode = mode;
-	my_timer.prescaler_frq = pres_frq;
-	my_timer.cmp_val = cmp_val;
-	my_timer.top_val = top_val;
-	my_timer.bot_val = bot_val;
-
-	return my_timer;
-}
-
 void init_timer(struct timer _timer){
        	
        	TOP    = (_timer.top_val > 0)? _timer.top_val : 0xFF;
        	BOTTOM = (_timer.bot_val < 0 && _timer.bot_val >= _timer.top_val )? 0x00 : _timer.bot_val;
-       	
-       	TCCR0A = 0; //clear 
-	TCCR0B = 0; //clear
-	TIMSKO = 0; //clear
-	TIFR0  = 0; //clear
-
-        // select prescaler
-        if(_timer.prescaler_frq == 5)       TCCR0B |= (1 << CS02) | (1 << CS00);
-	else if(_timer.prescaler_frq == 4)  TCCR0B |= (1 << CS02);
-	else if(_timer.prescaler_frq == 3)  TCCR0B |= (1 << CS01) | (1 << CS00);
-	else if(_timer.prescaler_frq == 2)  TCCR0B |= (1 << CS01);
-	else if(_timer.prescaler_frq == 1)  TCCR0B |= (1 << CS00);
-
-	      
+       	      
         if(_timer.sel == 0){
                 //first timer/counter 0
+	       	TCCR0A = 0; //clear 
+		TCCR0B = 0; //clear
+		TIMSKO = 0; //clear
+		TIFR0  = 0; //clear
+
+		// select prescaler
+		if(_timer.prescaler_frq == 5)       TCCR0B |= (1 << CS02) | (1 << CS00);
+		else if(_timer.prescaler_frq == 4)  TCCR0B |= (1 << CS02);
+		else if(_timer.prescaler_frq == 3)  TCCR0B |= (1 << CS01) | (1 << CS00);
+		else if(_timer.prescaler_frq == 2)  TCCR0B |= (1 << CS01);
+		else if(_timer.prescaler_frq == 1)  TCCR0B |= (1 << CS00);
+	       		
        		switch(_timer.mode){
        			case 0: //Normal mode
        				TCCR0A = 0x00; //set Normal Mode
@@ -65,13 +52,56 @@ void init_timer(struct timer _timer){
 			        break;
        		}
        		
-        	TIMSK0 |= (1 << OCIE0A);// Activ point comp OCR0A
-                sie(); // activate global interrupt
+        	TIMSK0 |= (1 << OCIE0A); // Activ point comp OCR0A
+                sie();                   // activate global interrupt
         }else if(_timer.sel == 1){
-            //16b timer/counter
+                //16b timer/counter
         
         }else if(_timer.sel == 2){
-          //second 8b timer/counter
+                //second 8b timer/counter 2
+	       	TCCR2A = 0; //clear 
+		TCCR2B = 0; //clear
+		TIMSK2 = 0; //clear
+		TIFR2  = 0; //clear
+
+		// select prescaler
+		if(_timer.prescaler_frq == 5)       TCCR2B |= (1 << CS22) | (1 << CS20);
+		else if(_timer.prescaler_frq == 4)  TCCR2B |= (1 << CS22);
+		else if(_timer.prescaler_frq == 3)  TCCR2B |= (1 << CS21) | (1 << CS20);
+		else if(_timer.prescaler_frq == 2)  TCCR2B |= (1 << CS21);
+		else if(_timer.prescaler_frq == 1)  TCCR2B |= (1 << CS20);
+	       		
+       		switch(_timer.mode){
+       			case 0: //Normal mode
+       				TCCR2A = 0x00; //set Normal Mode
+       					
+				OCR2A  = TOP; //cmp val is TOP 
+				break;
+			case 1: //PWM Phase Correct classic mode
+                                // PB1 (OC2A): output PWM for Timer2, ch A.
+                                // PB2 (OC2B): output PWM for Timer2, ch B.
+			        TCCR2A |= (1 << WGM20); // set pwm phase correct mode
+			        
+			        // Clear OC0A on compare match when up-counting. 
+			        // Set OC0A on compare match when down-countin
+			        TCCR2A |= (1 << COM2A1);
+			        
+			        // Set  OC0A (PD6) as output
+                                DDRD |= (1 << PB1);
+                                
+                                OCR2A = _timer.cmp_val;//set cmp value
+			        break;
+			case 2: //Clear Timer Control Mode
+			        TCCR2A |= (1 << WGM21); //set CTC Mode  
+			        
+			        OCR2A = _timer.cmp_val; //set cmp val
+			        break;
+			case 3: //Fast PWM mode
+			        break;
+       		}
+       		
+        	TIMSK2 |= (1 << OCIE2A); // Activ point comp OCR2A
+                sie();                   // activate global interrupt                
         }
 	
 }
@@ -80,9 +110,16 @@ void drive_timer(void (*timer_stuff)()){
 	timer_stuff();
 }
 
+struct timer* create(struct timer_config _conf){
+     struct timer timer;
+     timer.conf = _conf;
+     timer.init = &init_timer;
+     
+     return &timer;
+} 
+
 ISR(TIMER0_COMPA_vect) {
 	drive_timer(void (*timer_stuff)());
 }
-
 
 
--- atmega328p/Drivers/timer.h
+++ atmega328p/Drivers/timer.h
@@ -1,6 +1,6 @@
 #pragma once
 
-struct timer{
+struct timer_config{
 	uint8_t select;
 	uint8_t mode;
 	uint8_t prescaler_frq;
@@ -9,6 +9,12 @@ struct timer{
 	uint8_t bot_val;
 };
 
-struct timer create_struct_timer(uint8_t sel, uint8_t mode, uint8_t pres_frq, uint8_t cmp_val, uint8_t top_val, uint8_t bot_val);
+struct timer{
+        struct timer_config conf;	
+	void (*init)(struct timer);
+};
+
 void init_timer(struct timer _timer);
 void drive_timer(void (*timer_stuff)());
+
+struct timer* create(struct timer_config _conf);
