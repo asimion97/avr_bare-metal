af7e5afa86f675e811394f8319692096fbccfcdb
Andrei Simion <andrei.simion1707@upb.ro>
2023-06-09 16:34:49 +0300

    add i2c master write

--- atmega328p/Drivers/i2c.c
+++ atmega328p/Drivers/i2c.c
@@ -1,5 +1,52 @@
 #include "i2c.h"
 
+/////////////////AUX FUNCT FOR TRANSMISSION ////////////////////////////////////
+void _start_transmission(uint8_t _dest_addr, uint8_t direction) {
+
+    TWCR = (1 << TWSTA) ; // The application writes the TWSTA bit to one when it desires to become a master on the 2-wire serial bus.
+    
+    while (!(TWCR & (1 << TWINT)));
+    
+    if ((TWSR & MASK_TWSR) != START) ERROR();
+    
+    TWDR = ( _dest_addr << 1 ) | direction;
+    
+    if ((TWSR & MASK_TWSR) != MT_SLA_ACK) ERROR();
+    
+}
+
+void _transmission(uint8_t _data) {
+
+	TWDR = _data;
+	
+	while (!(TWCR & (1 << TWINT)));
+	
+	if ((TWSR & MASK_TWSR)!= MT_DATA_ACK) ERROR();
+	
+}
+
+void _stop_transmission() {
+	TWCR = (1 << TWSTO);
+}
+
+////////////////AUX FUNC FOR RECEPTION///////////////////////////////////////////
+uint8_t _reception() {
+    uint8_t data_rx;
+	
+	data_rx = TWDR;
+	
+	while (!(TWCR & (1 << TWINT)));
+	
+	if ((TWSR & MASK_TWSR)!= MT_DATA_ACK) ERROR();
+	
+	return data_rx;
+}
+
+void _stop_reception() {
+	
+}
+
+///////////////////// I2C init fct //////////////////////////////////////////////
 void init(struct i2c_conf *_conf) {
     //clear TWBR -- default value
     TWBR &= 0x00;
@@ -13,40 +60,61 @@ void init(struct i2c_conf *_conf) {
     //clear TWAR -- default value
     TWAR &= 0xff;
     
-    //enble I2C
+    //enble I2C + interrupts
     TWCR = (1 << TWEN) | (1 << TWIE);
     
-	if(_conf->mode == 0x01) { 
+	if( _conf->mode == 0x01 ) { 
+	//Master
 		//set prescaler
-    	if(_conf->prescaler > 0) TWSR = (_conf->prescaler << TWPS0);
+    	if( _conf->prescaler > 0 ) TWSR = ( _conf->prescaler << TWPS0 );
 	
 		//set I2C bit rate
-		TWBR = ((F_CPU / SCL_FREQUENCY) - 16) / 2;
+		TWBR = ((OSC_FRQ / SCL_FRQ) - 16) / 2;
 		
-	} else if (_conf->mode == 0x02) {
+	} else if ( _conf->mode == 0x02 ) {
+	//Slave
 		//set slave addr
-		TWAR = (_conf->slave_addr << 1);
+		TWAR = ( _conf->slave_addr << 1 );
 	}
 	
 }
 
-void start_transmission(uint8_t dest_addr) {
-	
+//////////// I2C master transmission func ////////////////////////////////////////
+void master_TX(uint8_t slave_addr, uint8_t data) {
+         _start_transmission(slave_addr, TX_BIT);
+          
+         _transmission(data);
+     
+         _stop_transmission();
 }
 
-void transmission(uint8_t data) {
-	
+void master_TX_Nbytes(uint8_t slave_addr, uint8_t* data, uint8_t n_bytes) {
+         _start_transmission(slave_addr, TX_BIT);
+          
+          for(int i = 0; i < n_bytes; i++) _transmission(*(data + i));
+     
+         _stop_transmission();
 }
 
-void stop_transmission() {
-	
+//////////// I2C master reception func //////////////////////////////////////////
+uint8_t master_RX(uint8_t slave_addr) {
+     _start_transmission(slave_addr, RX_BIT);
+     
+	 uint8_t i2c_rx = _reception();
+	 
+	 _stop_transmission();
+	 
+	 return i2c_rx;
 }
 
-
-
-
-struct i2c* contruct_i2c(struct i2c_conf* config) {
-	
+///////////////// CONSTRUCT I2C Structure ///////////////////////////////////////
+struct i2c* contruct_i2c() {
+	struct i2c* i2c_device   = (struct i2c*)malloc(struct i2c);
+	i2c_device->init         = init;
+	i2c_device->write        = master_TX;
+	i2c_device->write_nbytes = master_TX_Nbytes;
+	i2c_device->read         = master_RX;
+	return i2c_device;
 }
 
 
--- atmega328p/Drivers/i2c.h
+++ atmega328p/Drivers/i2c.h
@@ -2,25 +2,30 @@
 #include<stdlib.h>
 #include<stdint.h>
 
+#define OSC_FRQ   16000000UL
+#define SCL_FRQ     100000UL
+#define MASK_TWSR       0xF8
+#define TX_BIT             0
+#define RX_BIT             1
 
 struct i2c_conf {
+        //Mode: Master or Slave
 	uint8_t mode;
+	//Prescaler: division
 	uint8_t prescaler;
+	//Slave Address
 	uint8_t slave_addr;
 };
 
 struct i2c {
         //Init
-	void    (*i2c_init)(struct i2c_conf*);
+	void    (*init)(struct i2c_conf*);
 	// Transmission
-	void    (*i2c_start_tx)(uint8_t); // start transmission 
-	void    (*i2c_write)(uint8_t);    // write data to slave
-	void    (*i2c_stop_tx)(void);     // stop transmission
+	void    (*write)(uint8_t, uint8_t); // transmission 
+	void    (*write_nbytes)(uint8_t, uint8_t*, uint8_t);
 	// Reception
-	void    (*i2c_start_rx)(uint8_t); // start recv
-	void    (*i2c_read)(uint8_t);     // read data from slave
-	void    (*i2x_stop_rx)(uint8_t);  // stop recv
+	uint8_t (*read)(void);       // read data from slave
 };
 
 //construct i2c interface
-struct i2c* contruct_i2c(struct i2c_conf* config);
+struct i2c* contruct_i2c();
