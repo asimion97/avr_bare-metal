6bdefacb7938b5a2d2b5ac612b958eec91c78af2
Andrei Simion <andrei.simion1707@upb.ro>
2023-06-15 15:36:44 +0300

    wip bug fixing i2c

--- atmega328p/Drivers/i2c.c
+++ atmega328p/Drivers/i2c.c
@@ -2,17 +2,28 @@
 
 /////////////////AUX FUNCT FOR TRANSMISSION ////////////////////////////////////
 uint8_t _start_transmission(uint8_t _dest_addr, uint8_t direction) {
-
-    TWCR = (1 << TWSTA) ; // The application writes the TWSTA bit to one when it desires to become a master on the 2-wire serial bus.
+    //TWCR &= ~(1 << TWSTO);
+    //TWCR |= (1 << TWSTA) ; // The application writes the TWSTA bit to one when it desires to become a master on the 2-wire serial bus.
+   // TWCR |= (1<<TWINT);
+     TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
+    uint8_t twcr = TWCR;
+    Serial.println(twcr);
     
     while (!(TWCR & (1 << TWINT)));
     
-    if ((TWSR & MASK_TWSR) != TW_START) return TW_BUS_ERROR;
+    Serial.println("Start sent");
     
-    TWDR = ( _dest_addr << 1 ) | direction;
+    if ((TWSR & MASK_TWSR) != TW_START) return TW_BUS_ERROR;
+    Serial.println("Start ACK");
+     
+    TWDR |= ( _dest_addr << 1 ) | direction;
+    TWCR = (1 << TWINT) | (1 << TWEN);
+    
+    while (!(TWCR & (1 << TWINT)));
+    Serial.println("ADDR sent");
     
     if ((TWSR & MASK_TWSR) != TW_MT_SLA_ACK) return TW_BUS_ERROR;
-    
+    Serial.println("OK");
     return I2C_OK;
 }
 
@@ -29,7 +40,10 @@ uint8_t _transmission(uint8_t _data) {
 }
 
 void _stop_transmission() {
-	TWCR = ~(1 << TWSTA) | (1 << TWSTO);
+        //TWCR |=  (1 << TWINT);
+	//TWCR &= ~(1 << TWSTA);
+	//TWCR |=  (1 << TWSTO);
+	TWCR = (1<<TWINT)|(1<<TWEN)| (1<<TWSTO);
 }
 
 ////////////////AUX FUNC FOR RECEPTION///////////////////////////////////////////
@@ -51,6 +65,7 @@ void _stop_reception() {
 
 ///////////////////// I2C init fct //////////////////////////////////////////////
 void init(struct i2c_config *_conf) {
+    uint8_t psclr_v = 1;
     //clear TWBR -- default value
     TWBR &= 0x00;
     
@@ -64,25 +79,33 @@ void init(struct i2c_config *_conf) {
     TWAR |= 0xff;
     
     //enble I2C + interrupts
-    TWCR = (1 << TWEN);
+    TWCR |= (1 << TWEN);
     
-	if( _conf->mode == 0x01 ) { 
-	//Master
-	TWCR = (1 << TWINT);
-		//set prescaler
-    	if( _conf->prescaler > 0 ) TWSR = ( _conf->prescaler << TWPS0 );
+    if( _conf->mode == 0x01 ) { 
+        //Master transmiter
+	//set prescaler
+    	if( _conf->prescaler > 0 ) TWSR |= ( _conf->prescaler << TWPS0 );
 	
-		//set I2C bit rate
-		TWBR = ((OSC_FRQ / SCL_FRQ) - 16) / 2;
-		
-	} else if ( _conf->mode == 0x02 ) {
-	//Slave
-		//set slave addr
-		//TWAR = ( _conf->slave_addr << 1 );
-		TWAR |= _conf->slave_addr;
-		TWCR = (1 << TWEA);
-	}
+	//set I2C bit rate
+	TWBR |= ((OSC_FRQ / SCL_FRQ) - 16) / (2 * psclr_v);
 	
+	DDRC |= (1 << DDC4); // set pin SDA out
+	DDRC |= (1 << DDC5); // set pin SCL out
+	
+    } else if ( _conf->mode == 0x02 ) {
+	//Slave receptor
+	//set slave addr
+	//TWAR = ( _conf->slave_addr << 1 );
+	TWAR |= _conf->slave_addr;
+	TWCR |= (1 << TWEA);
+	
+	DDRC &= ~(1 << DDC4); // set pin SDA in
+	DDRC &= ~(1 << DDC5); // set pin SCL in
+    }
+	
+    // Activ internal pull-up
+    PORTC |= (1 << PORTC4); // SDA
+    PORTC |= (1 << PORTC5); // SCL
 }
 
 //////////// I2C master transmission func ////////////////////////////////////////
@@ -126,15 +149,19 @@ struct i2c* construct_i2c() {
 ///////////////// SCANNER I2C Slave ///////////////////////////////////////
 uint8_t scan_i2c_slave() {
 	uint8_t addr = 0xff;
-	for(int i = 1; i < 127; i++){
-	    uint8_t status = _start_transmission(i, RX_BIT);
+	for(uint8_t addr_i = 0x01 ; addr_i < 0x7f; addr_i++){
+	    Serial.print(addr_i);
+	    Serial.print(" ");
+	    uint8_t status = _start_transmission(addr_i, RX_BIT);
+	    
 	    if(status == I2C_OK) 
-	    {
-	    	addr = i;
+	    {Serial.println(status);
+	    	addr = addr_i;
 	    	_stop_transmission();
 	    	break;
 	    }
-	    _stop_transmission();	    
+	    _stop_transmission();
+	    delay(1000);	    
 	}
 	return addr;
 }
